33b0f88298e5560ade38c1ee549e76c5c46fa70d
mahdiAbu3li <62286695+mahdiAbu3li@users.noreply.github.com>
2020-04-08 03:31:52 +0300

    added all folder

---
+++ sign-in.php
@@ -0,0 +1,183 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no ">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost:8080/Transfer/style-loginAndSign.css">
+
+    <title>PS-progect</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    
+
+    
+
+        <a class="navbar-brand" href="homePage.html">MSHUARI.PS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
+
+            <div class="navbar-nav ">
+                <a class="nav-item nav-link active" href="homePage.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Contests</a>
+
+            </div>
+
+            <div class="navbar-nav">
+                <a id="nav-login" class="nav-item nav-link  bg-success rounded  " href="login-ps.html">Login</a>
+                <a id="nav-sign" class="nav-item nav-link bg-primary rounded " href="sign-in.php">Sign up</a>
+
+            </div>
+        </div>
+
+
+    </nav>
+
+
+    <div class="container h-50" style="margin-top: 100px ">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-12 col-sm-8 col-md-6  ">
+                <div class="card mb-4">
+                    <div class="card-header bg-primary">
+                        <h4>SIGN UP</h4>
+                    </div>
+                    <div class="card-body">
+
+
+                        <form>
+
+                            <div class="form-row form-group">
+                                <div class="col ">
+
+                                    <label for="exampleInputEmail1">First Name</label>
+                                    <input type="text" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="first name">
+
+                                </div>
+                                <div class="col">
+
+
+                                    <label for="exampleInputEmail1">Last Name</label>
+                                    <input type="text" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="last name">
+
+                                </div>
+                            </div>
+                            <div class="form-group">
+
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Password</label>
+                                <input type="password" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Password">
+                            </div>
+                            <div class="form-group">
+
+                                <label for="exampleInputEmail1">Confirm Password</label>
+                                <input type="password" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="rewrite your password">
+                            </div>
+                            <div class="form-group ">   
+                            
+
+                            <label for="exampleInputEmail1">Your Phone Number</label>
+                            <input type="text" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="EX:059x-xx-xx-xx">
+                        </div>
+
+                            <div class="form-row">
+                                <div class="col-3 ">
+
+                                    <label for="day">Day</label>
+                                    <select id="day" class="form-control form-control-sm">
+                                        <option selected>Day</option>
+                                        <?php
+                                        
+                                        for($i = 1 ; $i < 32 ; $i++){
+                                            echo "<option>$i</option>";
+                                        }
+                                        ?>
+                                       
+                                    </select>
+                                </div>
+                                <div class="col-3">
+                                    <label for="mounth">Mounth</label>
+                                    <select id="mounth" class="form-control form-control-sm">
+                                        <option selected>Mounth</option>
+                                        <?php
+                                        $months = array(
+                                            'January',
+                                            'February',
+                                            'March',
+                                            'April',
+                                            'May',
+                                            'June',
+                                            'July ',
+                                            'August',
+                                            'September',
+                                            'October',
+                                            'November',
+                                            'December',
+                                        );
+                                        for($i = 0 ; $i < 12 ; $i++){
+                                            echo "<option>".$months[$i]."</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="col-3">
+                                    <label for="year">Year</label>
+                                    <select id="year" class="form-control form-control-sm">
+                                        <option selected>Year</option>
+                                        <?php
+                                        for($i = 1900 ; $i < 2020 ; $i++){
+                                            echo "<option>$i</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                            </div>
+                            
+                           
+
+                           
+                            <div class="form-row mt-4">
+                                <div class="col-3 "></div>
+                                <div class="col-6">
+                                    <button type="submit" class="btn btn-primary btn-block mt-2">Sign UP </button>
+
+                                    <a id="s" href="login-ps.html" class="btn  btn-block mt-2 ">LOGIN</a>
+                                </div>
+                                <div class="col-3"></div>
+
+                            </div>
+                            
+
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
